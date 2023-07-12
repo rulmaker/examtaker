eec9f4944feeb34640cea7a3d06c2d8db0520a6c
rulmaker <rauljaciel_91@hotmail.com>
2023-07-11 21:25:10 -0600

    feature: project structure created

---
+++ src/main/java/com/rjsm/exam_taker_api/ExamTakerApiApplication.java
@@ -0,0 +1,13 @@
+package com.rjsm.exam_taker_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamTakerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamTakerApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rjsm/exam_taker_api/ExamTakerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rjsm.exam_taker_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamTakerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
